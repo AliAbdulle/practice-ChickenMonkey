bf4f2afa5c7063ab07f26d9975ec5992ba6f9001
AliAbdulle <alidhobey@gmail.com>
2019-02-28 15:38:38 -0600

    Added fixed error of the main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
     if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
-        console.log(ChickenMonkey) // Only 2, 4, 6 will appear
+        console.log('ChickenMonkey') // both will appear
     }
     // Divide the current number by 2, and check if the remainder is 0
     else if (currentNumber % 5 === 0) {
-        console.log(Chicken) // Only 2, 4, 6 will appear
+        console.log('Chicken') // Only 5 will appear
     }
     else if (currentNumber % 7 === 0) {
-        console.log(Monkey) // Only 2, 4, 6 will appear
+        console.log('Monkey') // Only 7 will appear
     } 
     else {
         console.log(currentNumber)
